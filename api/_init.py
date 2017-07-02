8a28e932a03f108d31fedee0a79d6fc1a4de9502
Muhammad Zaheer <smzaheerabbas@gmail.com>
2017-07-02 02:29:49 -0700

    Removed licensing information from _init.py Added a few directories to be used as a staging areas while processing the input video

--- api/_init.py
+++ api/_init.py
@@ -1,11 +1,4 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
-"""Set up paths for Fast R-CNN."""
+"""Set up paths for Faster R-CNN."""
 import os.path as osp
 import sys
 
