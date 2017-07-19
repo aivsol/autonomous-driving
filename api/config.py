d32c4324564975e1dc5b70c19da9ed82f01ca113
Muhammad Zaheer <smzaheerabbas@gmail.com>
2017-07-19 04:19:31 -0700

    Reorganized resources directory - removed unnecessary files

--- api/config.py
+++ api/config.py
@@ -14,8 +14,6 @@ __C.template_directory = os.path.join(os.getcwd(), "api/templates")
 
 __C.upload_folder = 'api/uploads/'
 
-__C.input_path = 'api/uploads/test.avi'
-
 __C.result_folder = 'api/uploads/result/'
 
 __C.frames_folder = 'api/uploads/frames'
@@ -24,18 +22,17 @@ __C.annotated_frames_folder = 'api/uploads/annotated-frames'
 
 __C.allowed_extensions = set(['avi'])
 
-__C.sign_prototxt = os.path.join(__C.root_directory, "api/"\
-                    "resources/models/gtsdb/ZF/faster_rcnn_end2end/"\
-                    "test.prototxt")
+__C.sign_prototxt = os.path.join(__C.root_directory, "api/resources/"\
+                            "faster-rcnn/models/gtsdb/ZF/test.prototxt")
 
 __C.sign_caffemodel = os.path.join(__C.root_directory, "api/"\
-                    "resources/weights/GTSDB/"\
+                    "resources/faster-rcnn/weights/GTSDB/"\
                     "zf_faster_rcnn_final.caffemodel")
 
 __C.vehicle_prototxt = os.path.join(__C.root_directory, "api/"\
-                    "resources/models/pascal_voc/ZF/faster_rcnn_alt_opt/"\
-                    "faster_rcnn_test.pt")
+                    "resources/faster-rcnn/models/pascal_voc/"\
+                    "ZF/test.prototxt")
 
 __C.vehicle_caffemodel = os.path.join(__C.root_directory, "api/"\
-                    "resources/weights/faster_rcnn_models/"\
+                    "resources/faster-rcnn/weights/pascal_voc/"\
                     "ZF_faster_rcnn_final.caffemodel")
